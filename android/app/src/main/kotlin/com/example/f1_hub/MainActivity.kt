dd1dcf7c40c1dcaabea07693b37b0c6e1f023528
netcrawlerr <abdulmejidamk@gmail.com>
2025-06-17 16:15:34 +0300

    chore: init

---
+++ android/app/src/main/kotlin/com/example/f1_hub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.netcrawler.f1_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:f1_hub/core/bottom_nav_bar.dart';
+import 'package:f1_hub/core/styles/app_styles.dart';
+import 'package:f1_hub/providers/theme_provider.dart';
+import 'package:f1_hub/screens/driver/driver_screen.dart';
+import 'package:f1_hub/screens/news/news_screen.dart';
+import 'package:f1_hub/screens/schedule/schedule_screen.dart';
+import 'package:f1_hub/screens/settings/settings_screen.dart';
+import 'package:f1_hub/screens/standing/standing_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ThemeProvider())],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: themeProvider.themeMode,
+
+      // Light Theme
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: AppStyles.bgColorLight,
+        primaryColor: Colors.green,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black, fontSize: 30),
+          bodyMedium: TextStyle(color: Colors.black87),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.green,
+            textStyle: const TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
+
+      // Dark Theme
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppStyles.bgColorDark,
+        primaryColor: const Color(0xFF0F3D1E),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white, fontSize: 30),
+          bodyMedium: TextStyle(color: Colors.white70),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.greenAccent,
+            textStyle: const TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
+
+      // Routes
+      routes: {
+        "/": (context) => BottomNavBar(),
+        "/news": (context) => NewsScreen(),
+        "/schedule": (context) => ScheduleScreen(),
+        "/standing": (context) => StandingScreen(),
+        "/driver": (context) => DriverScreen(),
+        "/settings": (context) => SettingsScreen(),
+      },
+    );
+  }
+}
